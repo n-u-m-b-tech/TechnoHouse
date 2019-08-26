255acc6a3777a63c7ad17b4b21e1ab38d54e11b8
MAISHA ABEL CHUENE <216077382@student.uj.ac.za>
2019-08-26 10:52:04 +0200

    Resolved

--- TechnoService/Techno_Service/ITechno_service.cs
+++ TechnoService/Techno_Service/ITechno_service.cs
@@ -43,7 +43,7 @@ namespace Techno_Service
 
         //shushan code
         [OperationContract]
-        int AddProduct(int ID);
+        int AddProduct(Product addP);
         [OperationContract]
         int EditProduct(int ID);
         [OperationContract]
