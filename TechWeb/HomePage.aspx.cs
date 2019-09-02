c94c74366a44d327db4ac75a546ad9fb3cc2c2c4
BRANDON BLESSING NYATHI <216089105@student.uj.ac.za>
2019-09-02 16:28:47 +0200

    added functionality on home page

--- TechWeb/HomePage.aspx.cs
+++ TechWeb/HomePage.aspx.cs
@@ -11,8 +11,10 @@ namespace TechWeb
     public partial class HomePage : System.Web.UI.Page
     {
         Techno_serviceClient client = new Techno_serviceClient();
+        String display = "";
         protected void Page_Load(object sender, EventArgs e)
         {
+            //hiding edit product hypelink if no admin is logged in
             edit.Visible = false;
             register.Visible = false;
             if (Session["userID"] != null)
@@ -21,34 +23,59 @@ namespace TechWeb
                 {
                     if (Session["userType"].Equals("admin"))
                     {
+                        //show hidden hyper links if logged in user is admin
                         edit.Visible = true;
                     }
                 }
                 login.Visible = false;
                 uregister.Visible = false;
             }
-            dynamic products = client.allProducts();
-            String display="";
 
-            foreach(ProductD p in products)
+            //displaying only two products per catergory
+            dynamic LivingRoom = client.search_by_cat("Living Room");
+            dynamic BathRoom = client.search_by_cat("Bath Room");
+            dynamic KitchenRoom = client.search_by_cat("Kitchen Room");
+            dynamic BedRoom = client.search_by_cat("Bed Room");
+            if (LivingRoom != null)
             {
-                display += "<article class='style1'>";
-                display +="<span class='image'>"; 
-                display += "<a href='EditProductPage.aspx?ID="+p.ID+"'>";
-                display +="<img src='"+p.image_url+"' width='300' height='300' alt='' />";
-                display += "</a>";
-                display += "</span>";
-                display += "<h2>"+p.name+"</h2>";
-                display += "<div class='content'>";
-                String price = "Price R" + p.price;
-                display += "<p>"+price+"</p>";
-                display += "</div>";
-                display += "</article>";
-            };
+                displaypro(LivingRoom);
+            }
+            if (BathRoom != null)
+            {
+               // displaypro(BathRoom);
+            }
+            if (KitchenRoom != null)
+            {
+                //displaypro(KitchenRoom);
+            }
+            if (BedRoom != null)
+            {
+                //displaypro(BedRoom);
+            }
+
+
+
 
             catalog.InnerHtml = display;
         }
 
-       
+        public void displaypro(dynamic product)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                display += "<article class='style1'>";
+                display += "<span class='image'>";
+                display += "<a href='EditProductPage.aspx?ID=" + product[i].ID + "'>";
+                display += "<img src='" + product[i].image_url + "' width='300' height='300' alt='' />";
+                display += "</a>";
+                display += "</span>";
+                display += "<h2>" + product[i].name + "</h2>";
+                display += "<div class='content'>";
+                String price = "Price R" + product[i].price;
+                display += "<p>" + price + "</p>";
+                display += "</div>";
+                display += "</article>";
+            }
+        }
     }
 }
