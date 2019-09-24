577ff0846e734f131bf80c3ede3947ee7e8a3b82
BRANDON BLESSING NYATHI <216089105@student.uj.ac.za>
2019-09-24 14:09:04 +0200

    changed little bit of code just a little bit

--- FinalWeb/Home.aspx.cs
+++ FinalWeb/Home.aspx.cs
@@ -14,17 +14,17 @@ namespace FinalWeb
         String display = "";
         protected void Page_Load(object sender, EventArgs e)
         {
-            dynamic LivingRoom = client.search_by_cat("Living Room");
-            dynamic BathRoom = client.search_by_cat("Bath Room");
-            dynamic KitchenRoom = client.search_by_cat("Kitchen");
-            dynamic BedRoom = client.search_by_cat("Bed Room");
+            ProductD[] LivingRoom = client.search_by_cat("Living Room");
+            ProductD[] BathRoom = client.search_by_cat("BathRoom");
+            ProductD[] KitchenRoom = client.search_by_cat("Kitchen");
+            ProductD[] BedRoom = client.search_by_cat("BedRoom");
             if (LivingRoom != null)
             {
-                Display(LivingRoom);
+              Display(LivingRoom);
             }
             if (BathRoom != null)
             {
-                Display(BathRoom);
+               Display(BathRoom);
             }
             if (KitchenRoom != null)
             {
@@ -38,12 +38,13 @@ namespace FinalWeb
             view.InnerHtml = display;
         }
 
-        public void Display(dynamic product) {
+        public void Display(ProductD[] product) {
+           
             for (int i = 0; i < 2; i++) {
                // var newPrice = product.price + (product.price*product.discount);
                 display += "<div class='block2'>";
                 display += "<div class='block2-img wrap-pic-w of-hidden pos-relative block2-labelsale'>";
-                display += "<img src ='"+ product.image_url +"' alt='IMG-PRODUCT'>";
+                display += "<img src ='"+ product[i].image_url +"' alt='IMG-PRODUCT'>";
                 display += "<div class='block2-overlay trans-0-4'>";
                 display += "<a href = '#' class='block2-btn-addwishlist hov-pointer trans-0-4'>";
 	  		    display += "<i class='icon-wishlist icon_heart_alt' aria-hidden='true'></i>";
@@ -55,12 +56,13 @@ namespace FinalWeb
                 display += "</div>";
                 display += "</div>";
                 display += "<div class='block2-txt p-t-20'>";
-				display += "<a href= 'ProductDetails.aspx?ID="+ product.ID+"' class='block2-name dis-block s-text3 p-b-5'>"+product.name+"</a>";
-				display += "<span class='block2-oldprice m-text7 p-r-5'>"+product.price+"</span>";
+				display += "<a href= 'ProductDetails.aspx?ID="+ product[i].ID+"' class='block2-name dis-block s-text3 p-b-5'>"+product[i].name+"</a>";
+				display += "<span class='block2-oldprice m-text7 p-r-5'>"+product[i].price+"</span>";
 				//display += "<span class='block2-newprice m-text8 p-r-5'>"+newPrice+"</span>";
 				display += "</div>";
 				display += "</div>";
-            }         
+            }
+            
         }
     }
 }
