2fc91ad6f14bde67eab8c20e9dc5be188cf1ba89
NKOSINATHI BETHUEL NDHLOVU <216025981@student.uj.ac.za>
2019-09-24 14:15:23 +0200

    .

--- FinalWeb/Home.aspx.cs
+++ FinalWeb/Home.aspx.cs
@@ -41,7 +41,7 @@ namespace FinalWeb
         public void Display(ProductD[] product) {
            
             for (int i = 0; i < 2; i++) {
-               // var newPrice = product.price + (product.price*product.discount);
+                var newPrice = product[i].price + (product[i].price*Convert.ToDouble(product[i].discount));
                 display += "<div class='block2'>";
                 display += "<div class='block2-img wrap-pic-w of-hidden pos-relative block2-labelsale'>";
                 display += "<img src ='"+ product[i].image_url +"' alt='IMG-PRODUCT'>";
@@ -58,7 +58,7 @@ namespace FinalWeb
                 display += "<div class='block2-txt p-t-20'>";
 				display += "<a href= 'ProductDetails.aspx?ID="+ product[i].ID+"' class='block2-name dis-block s-text3 p-b-5'>"+product[i].name+"</a>";
 				display += "<span class='block2-oldprice m-text7 p-r-5'>"+product[i].price+"</span>";
-				//display += "<span class='block2-newprice m-text8 p-r-5'>"+newPrice+"</span>";
+				display += "<span class='block2-newprice m-text8 p-r-5'>"+newPrice+"</span>";
 				display += "</div>";
 				display += "</div>";
             }
