3dc87681d4713b88c5d674f4f9fe54c0d773a978
NKOSINATHI BETHUEL NDHLOVU <216025981@student.uj.ac.za>
2019-10-15 19:16:11 +0200

    comit

--- FinalWeb/MyAccount.aspx.cs
+++ FinalWeb/MyAccount.aspx.cs
@@ -4,15 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using FinalWeb.TechService;
 
 namespace FinalWeb
 {
-    public partial class UserAccount1 : System.Web.UI.Page
+    public partial class MyAccount : System.Web.UI.Page
     {
-        Techno_serviceClient client = new Techno_serviceClient();
         protected void Page_Load(object sender, EventArgs e)
         {
+
         }
     }
 }
