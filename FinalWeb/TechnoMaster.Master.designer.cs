03b7d25b7dee37f3d2e7b3658669e99dedfc4328
NKOSINATHI BETHUEL NDHLOVU <216025981@student.uj.ac.za>
2019-10-16 10:53:30 +0200

    comit

--- FinalWeb/TechnoMaster.Master.designer.cs
+++ FinalWeb/TechnoMaster.Master.designer.cs
@@ -94,6 +94,15 @@ namespace FinalWeb {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl logout;
         
         /// <summary>
+        /// walletshow control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl walletshow;
+        
+        /// <summary>
         /// cartshow control.
         /// </summary>
         /// <remarks>
